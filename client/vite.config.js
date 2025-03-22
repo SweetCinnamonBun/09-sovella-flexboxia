92c70528e8fa15931c0a46a0587c7ef3642c7660
Roman <roman.klemiato18@gmail.com>
2025-03-22 22:03:14 +0200

    deployment stuff

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/09-sovella-flexboxia",
   server: {
     port: 3100,
   }
